316229c233285835fcba722a44fdc86f3b007a57
Tony Thomas <antonyt@outlook.com>
2021-08-11 22:33:46 -0400

    add twilio bindings and test function

--- Function.fs
+++ Function.fs
@@ -1,76 +1,68 @@
 namespace BoomTime
 
 open FSharp.Data
-open System
-open System.IO
-open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
-open Newtonsoft.Json
 open Microsoft.Extensions.Logging
+open System
+open Twilio.Rest.Api.V2010.Account
+open Twilio.Types
 
 module Function =
 
     let apiBase = "http://statsapi.mlb.com"    
     [<Literal>]    
-    let schedule = "http://statsapi.mlb.com/api/v1/schedule?sportId=11"
-    type Schedule = JsonProvider<schedule>
+    let scheduleSample = "http://statsapi.mlb.com/api/v1/schedule?sportId=11"
+    type Schedule = JsonProvider<scheduleSample>
     [<Literal>]    
     let gameSample = "http://statsapi.mlb.com/api/v1.1/game/647409/feed/live"
     type Game = JsonProvider<gameSample>
     
-    let todaysSchedule = Schedule.GetSample()
+    let schedule = Schedule.GetSample()
     
+    [<Literal>]
     let bulls = "Durham Bulls Athletic Park"
     
-    let tryFindOngoingGameInVenue venueName =
-        todaysSchedule.Dates.[0].Games
-        |> Seq.tryFind (fun game -> game.Status.AbstractGameState = "Live" && game.Venue.Name = venueName)
-    
-    let getOngoingGameDetails venueName =
-        let gameDetails =
-            todaysSchedule.Dates.[0].Games
-            |> Seq.tryFind (fun game -> game.Status.AbstractGameState = "Live" && game.Venue.Name = venueName)    
-            |> Option.bind (fun game ->
-                let details = Game.Load($"{apiBase}{game.Link}")
-                if details.LiveData.Linescore.CurrentInning >= 8 then Some details
-                else None)
+    let tryFindOngoingGameAtVenue venueName =
+        let gameDetails = 
+            schedule.Dates.[0].Games
+            |> Seq.tryFind (fun game -> game.Status.AbstractGameState = "Live" && game.Venue.Name = venueName)
+            |> Option.map (fun game -> Game.Load($"{apiBase}{game.Link}"))
         gameDetails
         
-//    [<FunctionName("Function2")>]
-//    let boomTime([<TimerTrigger("0 */10 * * * 5,6")>] timer: TimerInfo, ILogger log)
-//        ()1Xu 
+//    [<FunctionName("BoomTime")>]
+//    let boomTime([<TimerTrigger("0 */10 * * * 5,6")>] timer: TimerInfo, log: ILogger) =
+//        let venue = "Durham Bulls Athletic Park"
+//        
+//        tryFindOngoingGameAtVenue venue
+//        |> Option.filter (fun g -> g.LiveData.Linescore.CurrentInning >= 8)
+//        |> Option.map (postMessage venue)
+//        |> ignore
+//        
+//        OkResult()
+
+    let formattedMessage (game: Game.Root) =
+        $"\n🎆BOOMTIME🎇\n{game.GameData.Teams.Home.Name} vs {game.GameData.Teams.Away.Name}\n{game.LiveData.Linescore.CurrentInningOrdinal} inning 🐶"
+    
+    let createSms (game: Game.Root) =
+        let fromPhone = Environment.GetEnvironmentVariable("TWILIO_FROM")
+        let toPhone = Environment.GetEnvironmentVariable("TWILIO_TO")
+        CreateMessageOptions(
+                    ``to``= PhoneNumber(toPhone),
+                    From = PhoneNumber(fromPhone),
+                    Body = formattedMessage game)
+
+    [<FunctionName("BindingTest")>]    
+    let testBinding
+        ([<HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)>]req: HttpRequest)
+        ([<TwilioSms(AccountSidSetting = "TWILIO_SID", AuthTokenSetting = "TWILIO_TOKEN")>]msg: outref<CreateMessageOptions>)
+        (log: ILogger) =
+    
+        let venue = req.Query.["venue"].ToString()
         
-//    [<FunctionName("Function")>]
-//    let run ([<HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)>]req: HttpRequest) (log: ILogger) =
-//        async {
-//            log.LogInformation("F# HTTP trigger function processed a request.")
-//
-//            let nameOpt = 
-//                if req.Query.ContainsKey(Name) then
-//                    Some(req.Query.[Name].[0])
-//                else
-//                    None
-//
-//            use stream = new StreamReader(req.Body)
-//            let! reqBody = stream.ReadToEndAsync() |> Async.AwaitTask
-//
-//            let data = JsonConvert.DeserializeObject<NameContainer>(reqBody)
-//
-//            let name =
-//                match nameOpt with
-//                | Some n -> n
-//                | None ->
-//                   match data with
-//                   | null -> ""
-//                   | nc -> nc.Name
-//            
-//            let responseMessage =             
-//                if (String.IsNullOrWhiteSpace(name)) then
-//                    "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-//                else
-//                    "Hello, " +  name + ". This HTTP triggered function executed successfully."
-//
-//            return OkObjectResult(responseMessage) :> IActionResult
-//        } |> Async.StartAsTask
+        msg <-
+            tryFindOngoingGameAtVenue venue
+            |> Option.map createSms
+            |> Option.toObj
+        
