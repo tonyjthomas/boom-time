e0ccffab3c326f2d0241e527b185dc8400bc195d
Tony Thomas <tonyjthomas@github.com>
2021-08-13 18:19:49 -0400

    return a response from manual endpoint

--- BoomTime/Function.fs
+++ BoomTime/Function.fs
@@ -1,6 +1,7 @@
 namespace BoomTime
 
 open FSharp.Data
+open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
@@ -52,21 +53,24 @@ module Function =
         
     let filterOngoingAndSend venue f =
         if mostRecentMessage.DateSent.Value > DateTime.Now.Subtract(TimeSpan.FromHours(1.0)) then
-            ()
+            None
         else                
             tryFindOngoingGameAtVenue venue
             |> Option.filter f
             |> Option.map createSms
             |> Option.map MessageResource.Create
-            |> ignore
         
     [<FunctionName("BoomTime")>]
     let boomTime([<TimerTrigger("0 */10 * * * 5,6")>] timer: TimerInfo) =
         filterOngoingAndSend bullsStadium (fun g -> g.LiveData.Linescore.CurrentInning >= 8)
+        |> ignore
 
     [<FunctionName("Manual")>]    
     let testBinding ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) =
         let venue = req.Query.["venue"].ToString()
-        filterOngoingAndSend venue (fun _ -> true)
+        match filterOngoingAndSend venue (fun _ -> true) with
+        | None ->  sprintf "No ongoing games found at %s" 
+        | Some s -> sprintf "Found ongoing game at %s and sent notification"
+        |> OkObjectResult
             
         
