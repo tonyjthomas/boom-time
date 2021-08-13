c07645a0690424b93f1939b981ddf3fdfed1e995
Tony Thomas <antonyt@outlook.com>
2021-08-13 15:45:37 -0400

    deploy instead of generating template

--- BoomTime.Deploy/Program.fs
+++ BoomTime.Deploy/Program.fs
@@ -1,30 +1,46 @@
+open System
 open Farmer
 open Farmer.Builders
 
+[<Literal>]
+let twilioSid = "twilioSid"
+[<Literal>]
+let twilioToken = "twilioToken"
+[<Literal>]
+let twilioTo = "twilioTo"
+[<Literal>]
+let twilioFrom = "twilioFrom"
+[<Literal>]
+let rgName = "boomtime"
+
 let vault = keyVault {
-    name "kv-boomtime"
+    name $"kv-{rgName}"
 }
 
 let func = functions {
-    name "func-boomtime"
-    service_plan_name "plan-boomtime"
-    link_to_keyvault vault.Name
-    secret_setting "twilioSid"
-    secret_setting "twilioToken"
-    secret_setting "twilioFrom"
-    secret_setting "twilioTo"
+    name                $"func-{rgName}"
+    service_plan_name   $"plan-{rgName}"
+    link_to_keyvault    vault.Name
+    secret_setting      twilioSid
+    secret_setting      twilioToken
+    secret_setting      twilioFrom
+    secret_setting      twilioTo
+    zip_deploy          "package.zip"
 }
+
 let deployment = arm {
     location Location.EastUS
-    name "boomtime"
     add_resources [ vault; func ]
 }
 
-printf "Generating ARM template..."
-deployment |> Writer.quickWrite "template"
-printfn "all done! Template written to template.json"
+let parameters =
+    [ ("twilioSid",     Environment.GetEnvironmentVariable twilioSid)
+      ("twilioToken",   Environment.GetEnvironmentVariable twilioToken)
+      ("twilioFrom",    Environment.GetEnvironmentVariable twilioFrom)
+      ("twilioTo",      Environment.GetEnvironmentVariable twilioTo) ]
 
-// Alternatively, deploy your resource group directly to Azure here.
-// deployment
-// |> Deploy.execute "farmer-resource-group" Deploy.NoParameters
-// |> printfn "%A"
+printf "Beginning ARM deployment..."
+match deployment |> Deploy.tryExecute rgName parameters with
+| Ok _ -> $"Deployment to resource group '{rgName}' completed."
+| Error e -> $"Deployment failed with error: {e}."
+|> printfn "%s"
