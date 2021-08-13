b8f2a962a95010a244f5dd8369bade23a38e0976
Tony Thomas <tonyjthomas@github.com>
2021-08-13 18:54:56 -0400

    Revert "return a response from manual endpoint"
    This reverts commit e0ccffab3c326f2d0241e527b185dc8400bc195d.

--- BoomTime/Function.fs
+++ BoomTime/Function.fs
@@ -1,7 +1,6 @@
 namespace BoomTime
 
 open FSharp.Data
-open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
@@ -53,24 +52,21 @@ module Function =
         
     let filterOngoingAndSend venue f =
         if mostRecentMessage.DateSent.Value > DateTime.Now.Subtract(TimeSpan.FromHours(1.0)) then
-            None
+            ()
         else                
             tryFindOngoingGameAtVenue venue
             |> Option.filter f
             |> Option.map createSms
             |> Option.map MessageResource.Create
+            |> ignore
         
     [<FunctionName("BoomTime")>]
     let boomTime([<TimerTrigger("0 */10 * * * 5,6")>] timer: TimerInfo) =
         filterOngoingAndSend bullsStadium (fun g -> g.LiveData.Linescore.CurrentInning >= 8)
-        |> ignore
 
     [<FunctionName("Manual")>]    
     let testBinding ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) =
         let venue = req.Query.["venue"].ToString()
-        match filterOngoingAndSend venue (fun _ -> true) with
-        | None ->  sprintf "No ongoing games found at %s" 
-        | Some s -> sprintf "Found ongoing game at %s and sent notification"
-        |> OkObjectResult
+        filterOngoingAndSend venue (fun _ -> true)
             
         
